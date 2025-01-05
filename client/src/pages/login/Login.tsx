42ecf123cca5d4a695ed518557fc26622633c3f5
Omerf130 <ofaclieru@gmail.com>
2025-01-05 20:35:06 +0200

    login and sign up pages

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="page">
+      <div className="cover">
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <div className="login-btn">Login</div>
 
-export default Login
+        <p className="text">Or Login Using</p>
+        
+        <div className="alt-login">
+          <div className="facebook"></div>
+          <div className="google"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./Register.scss";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="page">
+      <div className="cover">
+        <h1>Sign Up</h1>
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="E-Mail" />
+        <input type="password" placeholder="password" />
+        <div className="signup-btn">Sign Up</div>
+
+        <p className="text">Or Sign Up Using</p>
+        
+        <div className="alt-signup">
+          <div className="facebook"></div>
+          <div className="google"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Register
